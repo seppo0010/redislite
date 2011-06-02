2167474f25f8dbf0ba3526e62e07a7881d715e9f
Sebastian Waisbrot <seppo0010@gmail.com>
2011-06-02 20:58:06 -0300

    Fix return in extra string function

--- src/page_string.c
+++ src/page_string.c
@@ -116,7 +116,7 @@ static int add_extra_string(void *_cs, char *str, int length)
 			return next_page;
 		}
 	}
-	return 0;
+	return next_page;
 }
 
 int redislite_insert_string(void *_cs, char *str, int length, int* num)
