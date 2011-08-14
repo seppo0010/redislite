49cd76b847a394d059d4578b6c23606a64c77c89
Sebastian Waisbrot <seppo0010@gmail.com>
2011-08-14 18:23:49 -0300

    Add renamenx

--- src/page_index.h
+++ src/page_index.h
@@ -32,5 +32,6 @@ int redislite_delete_keys(void *_cs, int q, char **keys, size_t *lengths);
 int redislite_exists_key(void *_db, void *_cs, char *key, size_t length);
 int redislite_get_keys(void *_db, void *_cs, char *pattern, int pattern_len, int *number_of_keys_p, char ***keys_p, int **keys_length_p);
 int redislite_page_index_rename_key(void *_cs, char *src, size_t src_len, char *target, size_t target_len);
+int redislite_page_index_renamenx_key(void *_cs, char *src, size_t src_len, char *target, size_t target_len);
 
 #endif
